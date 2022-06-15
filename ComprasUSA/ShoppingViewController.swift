83e95af2f688069b65d977c5a597972c361c095c
Matheus Oliveira <cruz.matheus99@gmail.com>
2022-06-15 14:34:20 -0300

    Aplicativo finalizado.

---
+++ ComprasUSA/ShoppingViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  ComprasUSA
+//
+//  Created by Matheus Oliveira on 08/06/22.
+//
+
+import UIKit
+
+class ShoppingViewController: UIViewController {
+    
+    @IBOutlet weak var tfDolar: UITextField!
+    @IBOutlet weak var lbRealDescription: UILabel!
+    @IBOutlet weak var lbReal: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setAmount()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        tfDolar.resignFirstResponder()
+        setAmount()
+    }
+    
+    func setAmount() {
+        tc.shoppingValue = tc.convertToDouble(tfDolar.text!)
+        lbReal.text = tc.getFormattedValue(of: tc.shoppingValue * tc.dolar, withCurrency: "R$ ")
+        let dolar = tc.getFormattedValue(of: tc.dolar, withCurrency: "")
+        lbRealDescription.text = "Valor sem impostos (dólar \(dolar))"
+    }
+}
+
---
+++ ComprasUSA/UIViewController+TaxesCalculator.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewController+TaxesCalculator.swift
+//  ComprasUSA
+//
+//  Created by Matheus Oliveira on 08/06/22.
+//
+
+import UIKit
+
+extension UIViewController {
+    var tc: TaxesCalculator {
+        return TaxesCalculator.shared
+    }
+}
+
